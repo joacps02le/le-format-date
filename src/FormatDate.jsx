c0e43b0dd6936c651bfc9567e89f854ca9a92edf
joacps02le <jo.acps02le>
2017-06-27 10:11:54 +0200

    Initial commit

---
+++ src/FormatDate.jsx
@@ -0,0 +1,20 @@
+/**
+ * Created by informatica on 26/06/17.
+ */
+import react from 'react';
+
+class FormatDate extends React.Component {
+    constructor() {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <h1>hello</h1>
+            </div>
+        )
+    }
+}
+export default FormatDate;
