d624157b19b0d5f947e0057803767762e316a4b5
joacps02le <jo.acps02le>
2017-07-19 14:34:55 +0200

    finish datepiker with selectdate onclick

--- src/FormatDateDrownList.jsx
+++ src/FormatDateDrownList.jsx
@@ -20,6 +20,7 @@ class FormatDateDrownList extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleBadInput = this.handleBadInput.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
     handleChange(date) {
@@ -28,72 +29,63 @@ class FormatDateDrownList extends React.Component {
         });
     }
 
-
-    handleInputChange(date) {
+    handleInputChange(e) {
         debugger
-        console.log(`date=`, date);
-        console.log(`prueba2=`, document.getElementById('piker'));
-        console.log(`prueba3=`, this.state);
-        // const originalValue = ReactDOM.findDOMNode(this.refs.input).value;
-        const originalValue = document.getElementById('piker').value;
-        const inputValue = originalValue.replace(/(-|\/\/)/g, this.state.separator).slice(0,10);
+            const originalValue = e.target.value.concat(e.key);
+            const inputValue = originalValue.replace(/(-|\/\/)/g, this.state.separator).slice(0, 10);
 
-        let month, day, year;
-        if (dateFormat.match(/MM.DD.YYYY/)) {
-            if (!inputValue.match(/[0-1][0-9].[0-3][0-9].[1-2][0-9][0-9][0-9]/)) {
+            let month, day, year;
+            if (dateFormat.match(/MM.DD.YYYY/)) {
+                if (!inputValue.match(/[0-1][0-9].[0-3][0-9].[1-2][0-9][0-9][0-9]/)) {
+                    debugger;
+                    return this.handleBadInput(originalValue);
+                }
+
+                month = inputValue.slice(0, 2).replace(/[^0-9]/g, '');
+                day = inputValue.slice(3, 5).replace(/[^0-9]/g, '');
+                year = inputValue.slice(6, 10).replace(/[^0-9]/g, '');
+            } else if (dateFormat.match(/DD.MM.YYYY/)) {
+                if (!inputValue.match(/[0-3][0-9].[0-1][0-9].[1-2][0-9][0-9][0-9]/)) {
+                    debugger;
+                    return this.handleBadInput(originalValue);
+                }
+
+                day = inputValue.slice(0, 2).replace(/[^0-9]/g, '');
+                month = inputValue.slice(3, 5).replace(/[^0-9]/g, '');
+                year = inputValue.slice(6, 10).replace(/[^0-9]/g, '');
+            } else {
+                if (!inputValue.match(/[1-2][0-9][0-9][0-9].[0-1][0-9].[0-3][0-9]/)) {
+                    debugger;
+                    return this.handleBadInput(originalValue);
+                }
+
+                year = inputValue.slice(0, 4).replace(/[^0-9]/g, '');
+                month = inputValue.slice(5, 7).replace(/[^0-9]/g, '');
+                day = inputValue.slice(8, 10).replace(/[^0-9]/g, '');
+            }
+
+            const monthInteger = parseInt(month, 10);
+            const dayInteger = parseInt(day, 10);
+            const yearInteger = parseInt(year, 10);
+            if (monthInteger > 12 || dayInteger > 31) {
                 debugger;
                 return this.handleBadInput(originalValue);
             }
 
-            month = inputValue.slice(0,2).replace(/[^0-9]/g, '');
-            day = inputValue.slice(3,5).replace(/[^0-9]/g, '');
-            year = inputValue.slice(6,10).replace(/[^0-9]/g, '');
-        } else if (dateFormat.match(/DD.MM.YYYY/)) {
-            if (!inputValue.match(/[0-3][0-9].[0-1][0-9].[1-2][0-9][0-9][0-9]/)) {
-                debugger;
-                return this.handleBadInput(originalValue);
+            if (!isNaN(monthInteger) && !isNaN(dayInteger) && !isNaN(yearInteger) && monthInteger <= 12 && dayInteger <= 31 && yearInteger > 999) {
+                const selectedDate = new Date(yearInteger, monthInteger - 1, dayInteger, 12, 0, 0, 0);
+                this.setState({
+                    selectedDate: selectedDate,
+                });
+
+                if (this.props.onChange) {
+                    this.props.onChange(selectedDate.toISOString(), inputValue);
+                }
             }
 
-            day = inputValue.slice(0,2).replace(/[^0-9]/g, '');
-            month = inputValue.slice(3,5).replace(/[^0-9]/g, '');
-            year = inputValue.slice(6,10).replace(/[^0-9]/g, '');
-        } else {
-            if (!inputValue.match(/[1-2][0-9][0-9][0-9].[0-1][0-9].[0-3][0-9]/)) {
-                debugger;
-                return this.handleBadInput(originalValue);
-            }
-
-            year = inputValue.slice(0,4).replace(/[^0-9]/g, '');
-            month = inputValue.slice(5,7).replace(/[^0-9]/g, '');
-            day = inputValue.slice(8,10).replace(/[^0-9]/g, '');
-        }
-
-        const monthInteger = parseInt(month, 10);
-        const dayInteger = parseInt(day, 10);
-        const yearInteger = parseInt(year, 10);
-        if (monthInteger > 12 || dayInteger > 31) {
-            debugger;
-            return this.handleBadInput(originalValue);
-        }
-
-        if (!isNaN(monthInteger) && !isNaN(dayInteger) && !isNaN(yearInteger) && monthInteger <= 12 && dayInteger <= 31 && yearInteger > 999) {
-            const selectedDate = new Date(yearInteger, monthInteger - 1, dayInteger, 12, 0, 0, 0);
             this.setState({
-                selectedDate: selectedDate,
-                displayDate: selectedDate,
-                value: selectedDate.toISOString()
+                startDate: inputValue
             });
-
-            if (this.props.onChange) {
-                this.props.onChange(selectedDate.toISOString(), inputValue);
-            }
-        }
-
-        console.log(`inputValue=`, inputValue);
-        console.log(`date=`, date);
-        this.setState({
-            startDate: inputValue
-        });
     }
 
     handleBadInput(originalValue) {
@@ -109,7 +101,7 @@ class FormatDateDrownList extends React.Component {
                 parts[1] = parts[1].slice(0, 2);
             }
             if (parts[2]) {
-                parts[2] = parts[2].slice(0,4);
+                parts[2] = parts[2].slice(0, 4);
             }
         } else {
             if (parts[0] && parts[0].length > 4) {
@@ -121,7 +113,7 @@ class FormatDateDrownList extends React.Component {
                 parts[1] = parts[1].slice(0, 2);
             }
             if (parts[2]) {
-                parts[2] = parts[2].slice(0,2);
+                parts[2] = parts[2].slice(0, 2);
             }
         }
         this.setState({
@@ -129,6 +121,20 @@ class FormatDateDrownList extends React.Component {
         });
     }
 
+    handleKeyDown(e) {
+        if (e.key.match(/[0-9]/)) {
+            this.handleInputChange(e);
+        } else {
+            let key = e.keyCode || e.charCode;
+
+            if (key === 8 || key === 46) {
+                this.setState({
+                    startDate: ''
+                });
+            }
+        }
+        console.log(this.state.startDate);
+    }
 
     render() {
         console.log(`prueba =`, this.state);
@@ -136,12 +142,13 @@ class FormatDateDrownList extends React.Component {
         return (
             <div>
                 <DatePickerDrown
-                    id='piker'
                     selected={this.state.startDate}
-                    onChange={this.handleInputChange}
                     peekNextMonth
                     showMonthDropdown
                     showYearDropdown
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleKeyDown}
+                    value={this.state.startDate}
                     dropdownMode="select"
                 />
             </div>
